eaefd884b4a89b97d2fd8b0083a6878bafddf997
igor <ifigurin1411@gmail.com>
2022-09-18 14:57:12 +0300

    pochti

---
+++ Classes/CustomButton.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace TestTask.Classes
+{
+    public class CustomButton : System.Windows.Controls.Button
+    {
+        public int xCoords, yCoords;
+
+        public CustomButton(int xCoords, int yCoords)
+        {
+            this.xCoords = xCoords;
+            this.yCoords = yCoords;
+        }
+        public void changesValuesButton(CustomButton[,] buttonArrays)
+        {
+            string charOfButton;
+            for (int i = 0; i < buttonArrays.GetLength(0); i++)
+            {
+                charOfButton = (string)buttonArrays[i, yCoords].Content;
+                if (charOfButton == "|")
+                {
+                    buttonArrays[i, yCoords].Content = "-";
+                    buttonArrays[i, yCoords].Background = Brushes.Green;
+                }
+                else
+                {
+                    buttonArrays[i, yCoords].Content = "|";
+                    buttonArrays[i, yCoords].Background = Brushes.Blue;
+                }
+                charOfButton = (string)buttonArrays[xCoords, i].Content;
+                if (charOfButton == "|")
+                {
+                    buttonArrays[xCoords, i].Content = "-";
+                    buttonArrays[xCoords, i].Background = Brushes.Green;
+                }
+                else
+                {
+                    buttonArrays[xCoords, i].Content = "|";
+                    buttonArrays[xCoords, i].Background = Brushes.Blue;
+                }
+            }
+            charOfButton = (string)buttonArrays[xCoords, yCoords].Content;
+            if (charOfButton == "|")
+            {
+                buttonArrays[xCoords, yCoords].Content = "-";
+                buttonArrays[xCoords, yCoords].Background = Brushes.Green;
+            }
+            else { 
+                buttonArrays[xCoords, yCoords].Content = "|";
+                buttonArrays[xCoords, yCoords].Background = Brushes.Blue;
+            }
+        }
+        
+    } 
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TestTask.Classes;
 
 namespace TestTask
 {
@@ -21,21 +22,20 @@ namespace TestTask
     /// </summary>
     public partial class MainWindow : Window
     {
-        const char v = '|';
-        const char s = '-';
-        private Button[,] gameField;
+        private CustomButton[,] gameFieldCustom; 
         private int fieldSize;
+        
         public MainWindow()
         {
             InitializeComponent();
         }
+
         private void btnClearAndGenerate(object sender, RoutedEventArgs e)
         {
             fieldSize = int.Parse(sizeInit.Text);
             if (fieldSize > 1)
             {
-                bool isWin = false; 
-                gameField = GenerateGrid(gameFieldGrid, int.Parse(sizeInit.Text));
+                gameFieldCustom = (CustomButton[,])GenerateGrid(gameFieldGrid, int.Parse(sizeInit.Text));
             }
             else
             {
@@ -45,99 +45,78 @@ namespace TestTask
         }
 
 
-        private Button[,] GenerateGrid(UniformGrid Grid, int size)
+        private CustomButton[,] GenerateGrid(UniformGrid Grid, int size)
         {
             fieldSize = size;
-            Button[,] result = new Button[size, size];
-
+            CustomButton[,] result = new CustomButton[size, size];
             Grid.Rows = size;
             Grid.Columns = size;
             Grid.Children.Clear();
+            Random rand = new Random(DateTime.Now.Second * DateTime.Now.Millisecond);
 
-            int tempValue; 
+            int clickValue = rand.Next(3, 8);
 
             for (int i = 0; i < size; i++)
             {
                 for (int j = 0; j < size; j++)
                 {
-                    result[i, j] = new Button();
-                    tempValue = randomValues(); 
-                    if(tempValue == 0)
-                    {
-                        result[i, j].Content = "|";
-                    }
-                    else { result[i, j].Content = "-"; }
+                    result[i, j] = new CustomButton(i, j);
+                    result[i, j].Content = "|";
+                    result[i, j].Background = Brushes.Blue;
                     result[i, j].Click += btnFieldClick; 
                     Grid.Children.Add(result[i, j]);
                 }
             }
+            int x, y, countArr;
+            int[] xCoordsArr, yCoordsArr; 
+            while (clickValue > 0)
+            {
+                x = rand.Next(0, size);
+                y = rand.Next(0, size); 
+                result[x, y].changesValuesButton(result);
+                clickValue--;
+            }
+            
+
             return result;
         }
 
+
         private void btnFieldClick(object sender, RoutedEventArgs e)
         {
-            Button ourButton = sender as Button;
-            string equalsSymbol = (string)ourButton.Content; 
-            if (equalsSymbol == "|")
-            {
-                ourButton.Content = "-"; 
-            }
-            else
-            {
-                ourButton.Content = "|";
-            }
+            CustomButton ourButton = sender as CustomButton;
+            ourButton.changesValuesButton(gameFieldCustom);
         }
 
         private void btnCheckWin(object sender, RoutedEventArgs e)
         {
-
+            if(safeIsOpen(gameFieldCustom, "|") == true || safeIsOpen(gameFieldCustom, "-") == true)
+            {
+                MessageBox.Show("Вы выйграли!");
+                gameFieldGrid.Children.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Вы проиграли!");
+            }
+        }
+        public bool safeIsOpen(CustomButton[,] buttonArrays, string charEquals)
+        {
             bool isWin;
             string equalsSymbhol;
             isWin = true;
-            foreach (Button item in gameField)
+            foreach (Button item in buttonArrays)
             {
                 equalsSymbhol = (string)item.Content;
-                if (equalsSymbhol == "|")
+                if (equalsSymbhol == charEquals)
                 {
                     isWin = false;
                     break;
                 }
             }
-            if (isWin)
-            {
-                MessageBox.Show("вы выйграли!");
-            }
-            else
-            {
-                isWin = true;
-                foreach (Button item in gameField)
-                {
-                    equalsSymbhol = (string)item.Content;
-                    if (equalsSymbhol == "-")
-                    {
-                        isWin = false;
-                        break;
-                    }
-                }
-                if (isWin)
-                {
-                    MessageBox.Show("вы выйграли!");
-                }
-                else
-                {
-                    MessageBox.Show("Проебали!");
-                }
-            }
-            
-           
-
+            return isWin;
         }
 
-        private int randomValues()
-        {
-            Random rand = new Random(DateTime.Now.Second + DateTime.Now.Millisecond);
-            return rand.Next(0, 2);
-        }
     }
     
 }
